cc3068382da1211c9b3cd7e5a32bfe2e5a5807c6
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-01-25 22:29:03 +0100

    add basic temperature and humidity sensor generation

--- src/generator.rs
+++ src/generator.rs
@@ -1,3 +1,4 @@
+use crate::hass::Entity;
 use log::{debug, info};
 use sqlx::{query_file, PgConnection};
 use std::io::Write;
@@ -7,6 +8,7 @@ pub async fn run(
     verify: bool,
     skip: &[u16],
     mut db: PgConnection,
+    db_url: &str,
     out: impl Write,
 ) -> anyhow::Result<()> {
     let mut nodes = get_node_ids(&mut db).await?;
@@ -15,6 +17,21 @@ pub async fn run(
     debug!("Filtering nodes: {skip:?}");
     nodes.retain(|id| !skip.contains(id));
     info!("Working with nodes: {nodes:?}");
+
+    let mut entities = Vec::new();
+
+    // Temperature and Humidity are required, so we can do them here
+    for node_id in nodes {
+        debug!("Processing node #{node_id}");
+
+        let temperature = Entity::new_temperature(node_id, db_url);
+        let humidity = Entity::new_humidity(node_id, db_url);
+
+        entities.push(temperature);
+        entities.push(humidity);
+    }
+
+    serde_yaml::to_writer(out, &entities)?;
 
     Ok(())
 }
